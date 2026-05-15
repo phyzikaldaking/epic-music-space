a72cce5800d6dc0f4bbcdc5daac0b82a185b5fb8
William J Williams <phyzikaldaking@gmail.com>
2026-05-15 14:11:00 -0400

    feat: add EMS Guardian fps telemetry scaffold

---
+++ scripts/guardian/fps-telemetry.mjs
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+console.log('EMS Guardian FPS telemetry scaffold active.');
+console.log('Recommended browser metrics:');
+console.log('- average FPS');
+console.log('- dropped frames');
+console.log('- long tasks');
+console.log('- timeline scroll FPS');
+console.log('- mixer repaint FPS');
+process.exit(0);
