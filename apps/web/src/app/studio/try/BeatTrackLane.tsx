2eac725467d97473bb9df25711093d74eba1a34c
William J Williams <phyzikaldaking@gmail.com>
2026-05-16 15:12:14 -0400

    Wire sound browser props through beat lane

--- apps/web/src/app/studio/try/BeatTrackLane.tsx
+++ apps/web/src/app/studio/try/BeatTrackLane.tsx
@@ -5,24 +5,63 @@ import BeatPadGrid from "./BeatPadGrid";
 import BeatSequencerRow from "./BeatSequencerRow";
 import BeatTransport from "./BeatTransport";
 import VirtualTrackList from "./VirtualTrackList";
-import type { DrumKind } from "@/components/daw/beatMachine";
-import type { StudioMidiBridge, StudioPad, StudioTrack, StudioTrackKind } from "./studioWorkstationTypes";
+import type { DrumKind, DrumKitId } from "@/components/daw/beatMachine";
+import type { StudioMidiBridge, StudioPad, StudioSoundAsset, StudioTrack, StudioTrackKind } from "./studioWorkstationTypes";
 
 type Props = {
   tracks: StudioTrack[];
   pads: StudioPad[];
   activePad: string | null;
   selectedTrack: string;
+  selectedKit: DrumKitId;
+  selectedInstrument: string;
+  sounds: StudioSoundAsset[];
   midi: StudioMidiBridge;
   onFirePad: (kind: DrumKind, label: string) => void;
   onAddTrack: (kind?: StudioTrackKind) => void;
   onSelectTrack: (id: string) => void;
+  onKitChange: (kit: DrumKitId) => void;
+  onInstrumentChange: (instrument: string) => void;
+  onSoundUploaded: (sound: StudioSoundAsset) => void;
+  onDropSoundOnTimeline: (sound: StudioSoundAsset) => void;
+  onAssignSoundToTrack: (sound: StudioSoundAsset) => void;
+  notify: (message: string) => void;
 };
 
-function BeatTrackLane({ tracks, pads, activePad, selectedTrack, midi, onFirePad, onAddTrack, onSelectTrack }: Props) {
+function BeatTrackLane({
+  tracks,
+  pads,
+  activePad,
+  selectedTrack,
+  selectedKit,
+  selectedInstrument,
+  sounds,
+  midi,
+  onFirePad,
+  onAddTrack,
+  onSelectTrack,
+  onKitChange,
+  onInstrumentChange,
+  onSoundUploaded,
+  onDropSoundOnTimeline,
+  onAssignSoundToTrack,
+  notify,
+}: Props) {
   return (
     <section className="h-full min-h-0 overflow-hidden rounded-xl border border-green-300/20 bg-black/45 p-2">
-      <BeatTransport midi={midi} onAddTrack={onAddTrack} />
+      <BeatTransport
+        midi={midi}
+        selectedKit={selectedKit}
+        selectedInstrument={selectedInstrument}
+        sounds={sounds}
+        onKitChange={onKitChange}
+        onInstrumentChange={onInstrumentChange}
+        onSoundUploaded={onSoundUploaded}
+        onAddTrack={onAddTrack}
+        onDropSoundOnTimeline={onDropSoundOnTimeline}
+        onAssignSoundToTrack={onAssignSoundToTrack}
+        notify={notify}
+      />
       <div className="grid h-[calc(100%-180px)] min-h-[420px] min-w-0 grid-cols-1 gap-2 overflow-hidden xl:grid-cols-[210px_minmax(0,1fr)]">
         <div className="min-h-0 overflow-y-auto pr-1">
           <BeatPadGrid pads={pads} activePad={activePad} onFirePad={onFirePad} />
