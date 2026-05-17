bcd7a8eb7f88f87d7689d2f51459594f84bd6601
William J Williams <phyzikaldaking@gmail.com>
2026-05-17 15:54:29 -0400

    Add beat machine piano roll page

---
+++ apps/web/src/app/beat-machine/piano-roll/page.tsx
@@ -0,0 +1,5 @@
+import BeatMachineProClient from "../../studio/beat-machine/BeatMachineProClient";
+
+export default function BeatMachinePianoRollPage() {
+  return <BeatMachineProClient initialView="piano" />;
+}
