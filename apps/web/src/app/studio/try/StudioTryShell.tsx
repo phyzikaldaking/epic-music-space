be4b359d650719b2c9ea7d8d0ee90a4f66129067
William J Williams <phyzikaldaking@gmail.com>
2026-05-18 01:19:45 -0400

    Use mixer DAW editor in studio shell

--- apps/web/src/app/studio/try/StudioTryShell.tsx
+++ apps/web/src/app/studio/try/StudioTryShell.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 
 import BeatMachineProClient from "../beat-machine/BeatMachineProClient";
-import ElectricStudioRecordDaw from "./ElectricStudioRecordDaw";
+import ElectricStudioMixerDaw from "./ElectricStudioMixerDaw";
 import StudioCloudUploadPanel from "./StudioCloudUploadPanel";
 import StudioControlFixPanel from "./StudioControlFixPanel";
 import StudioMp3EncoderPanel from "./StudioMp3EncoderPanel";
@@ -48,7 +48,7 @@ export default function StudioTryShell() {
 
       <div className="h-full overflow-hidden pb-16 md:pb-0">
         {workspace === "daw" ? (
-          <ElectricStudioRecordDaw />
+          <ElectricStudioMixerDaw />
         ) : workspace === "controls" ? (
           <StudioControlFixPanel />
         ) : workspace === "beat" ? (
