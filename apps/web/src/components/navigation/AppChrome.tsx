64c6d8a569c96905cf2e0bd42427c4f7a3f44c0e
William J Williams <phyzikaldaking@gmail.com>
2026-05-17 21:55:12 -0400

    Remove duplicate app chrome navigation

--- apps/web/src/components/navigation/AppChrome.tsx
+++ apps/web/src/components/navigation/AppChrome.tsx
@@ -1,21 +1,5 @@
 "use client";
 
-import { usePathname } from "next/navigation";
-
-import EmsMainMenu from "@/components/navigation/EmsMainMenu";
-import EmsMobileBottomNav from "@/components/navigation/EmsMobileBottomNav";
-
-const fullscreenRoutes = new Set(["/studio/try"]);
-
 export default function AppChrome({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-  const fullscreen = fullscreenRoutes.has(pathname);
-
-  return (
-    <>
-      {!fullscreen && <EmsMainMenu />}
-      {children}
-      {!fullscreen && <EmsMobileBottomNav />}
-    </>
-  );
+  return <>{children}</>;
 }
