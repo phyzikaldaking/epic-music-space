45db99771217ae969581f912c64300203641cb8f
William J Williams <phyzikaldaking@gmail.com>
2026-08-25 19:16:23 -0400

    Track mixer channel changes

--- apps/web/src/app/studio/try/studio/components/MixerWorkspace.tsx
+++ apps/web/src/app/studio/try/studio/components/MixerWorkspace.tsx
@@ -3,6 +3,7 @@
 import type { StudioTrack } from "../types";
 import { EffectsBrowser } from "./EffectsBrowser";
 import { MixAssistant } from "./MixAssistant";
+import { trackStudio } from "../../../../lib/studioTelemetry";
 
 export function MixerWorkspace({
   tracks,
